05ce9420875d00411ff7bd71686bc6c3ab306812
stephdraggo <steph.valais@gmx.com>
2021-03-19 09:36:31 +1100

    Update Person.cs

--- Assets/BigBoi/DialogueSystem/Person.cs
+++ Assets/BigBoi/DialogueSystem/Person.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// This text is in the person class
+/// This text is in the person class above the namespace
 /// </summary>
 namespace BigBoi.DialogueSystem
 {
     [CreateAssetMenu(menuName ="BigBoi/Dialogue System/Person",fileName ="new person")]
+    /// <summary>
+    /// This text is in the person class above the class name
+    /// </summary>
     public class Person : ScriptableObject
     {
         [SerializeField] private Sprite sprite;
