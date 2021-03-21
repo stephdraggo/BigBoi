f6de4dbc47f7c5def963863910a65b3e5d5ea225
stephdraggo <61036246+stephdraggo@users.noreply.github.com>
2021-03-21 19:18:44 +1100

    Dialogue.cs commented

--- Assets/BigBoi/DialogueSystem/Dialogue.cs
+++ Assets/BigBoi/DialogueSystem/Dialogue.cs
@@ -2,12 +2,25 @@ using UnityEngine;
 
 namespace BigBoi.DialogueSystem
 {
+    /// <summary>
+    /// Holds an array of dialogue lines.
+    /// Allows finding the index of a specific line using LinearSearch() from SearchingAlgorithms.cs
+    /// </summary>
     [CreateAssetMenu(menuName = "BigBoi/Dialogue System/Dialogue", fileName = "new dialogue")]
     public class Dialogue : ScriptableObject
-    {[SerializeField]
+    {
+        /// <summary>
+        /// Array of dialogue lines.
+        /// </summary>
+        [SerializeField,Tooltip("Add lines to this dialogue set here.")]
         private Line[] lines;
         public Line[] Lines => lines;
-        public Line ThisLine(int _index) => lines[_index];
+
+        /// <summary>
+        /// Method for finding the index of a specific line in this dialogue.
+        /// </summary>
+        /// <param name="_line">line to locate</param>
+        /// <returns>index of passed line</returns>
         public int LineIndex(Line _line) => SearchingAlgorithms.LinearSearch(lines, _line);
     }
 }
